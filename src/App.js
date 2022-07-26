2d118bd7e49d936765ecb9efb302f560d2db6009
Parin Thalangdee <lotteman10@gmail.com>
2022-07-26 02:11:39 -0700

    Created projects object

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Navbar from './components/Navbar'
 import CssBaseline from '@mui/material/CssBaseline';
 import About from './components/About'
-import Work from './components/Work/Work'
+import Work from './components/Work'
 
 function App() {
   return (
--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -1,8 +1,51 @@
 import React from 'react'
+import Grid from '@mui/material/Grid';
+
 
 const Work = () => {
+  let projects = [{
+    title: "example",
+    github: "",
+    heroku: "",
+    image: ""
+  },
+  {
+    title: "example",
+    github: "",
+    heroku: "",
+    image: ""
+  },
+  {
+    title: "example",
+    github: "",
+    heroku: "",
+    image: ""
+  },
+  {
+    title: "example",
+    github: "",
+    heroku: "",
+    image: ""
+  },
+  {
+    title: "example",
+    github: "",
+    heroku: "",
+    image: ""
+  },
+  {
+    title: "example",
+    github: "",
+    heroku: "",
+    image: ""
+  }]
+
   return (
-    
+    <Grid>
+      <Grid justifyContent="center" container padding={2}>
+          <h1>My Work</h1>
+      </Grid>
+    </Grid>
   )
 }
 
